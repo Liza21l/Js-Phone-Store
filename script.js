0bb8be5cbcbd61694d0562b285eb9a2951e8bd31
Liza21l <Liza2009koshenya@gmail.com>
2023-05-09 18:37:05 +0300

    site

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ form.btn.addEventListener('click', () => {
     setBalance(balanceCurrent+valueAdd)
     form.input.value = ''
 })
+
+let summary = { 
+    value: document.querySelector('.summary'),
+    count: document.querySelector('.count'),
+    btns: { 
+        minus: document.querySelector('.btnMinus'),
+        plus: document.querySelector('.btnPlus')
+    }
+}
+
 let productsStore = [ 
     { 
         id: 1, 
@@ -43,10 +53,11 @@ let productsStore = [
     } 
  
 ] 
- 
- 
+
+
+
 let bucketStore = [] 
-  
+ 
 const removeProduct = (idToRemove) => { 
     productsStore = productsStore.filter(product => {
         if (product.id != idToRemove){
@@ -83,19 +94,57 @@ const generateProducts = () => {
             <div id="product-${product.id}" class="cards"> 
                 <img src="./samsung-a32.svg">
                 <p class="name">${product.name}</p> 
-                <p class="price">${product.price} грн</p> 
-                <button class="btnAdd" id="add-${product.id}">Add to bucket</button>
+                <p class="price" id="price-${product.id}">${product.price} грн</p>
+                <button class="btnAdd" id="add-${product.id}">Add to bucket</button> 
+                <div class="prices" id="price-${product.id}">
+                    <button class="btnMinus" id="btnMinus-${product.id}">-</button>
+                    <p class="count" id="count-${product.id}">Count: 1</p>
+                    <button class="btnPlus" id="btnPlus-${product.id}">+</button>
+                </div>
             </div> 
             ` 
         } 
     }) 
+    const btnsMinus = document.querySelectorAll('.btnMinus')
+    const btnsPlus = document.querySelectorAll('.btnPlus')
+    btnsPlus.forEach((btn) => {
+        btn.addEventListener('click', () => { 
+            let currentId = parseInt(btn.parentNode.id.substring(6))
+            let price = productsStore[findItemInArray(productsStore, currentId)].price
+            if (productsStore[findItemInArray(productsStore, currentId)].count) {
+                productsStore[findItemInArray(productsStore, currentId)].count += 1
+            } else { 
+                productsStore[findItemInArray(productsStore, currentId)].count = 2
+            }
+            document.querySelector(`#price-${currentId}`).innerHTML = price * productsStore[findItemInArray(productsStore, currentId)].count
+            document.querySelector(`#count-${currentId}`).innerHTML = `Count: ${productsStore[findItemInArray(productsStore, currentId)].count}`
+        })
+    })
+    btnsMinus.forEach((btn) => {
+        btn.addEventListener('click', () => { 
+            
+            let currentId = parseInt(btn.parentNode.id.substring(6))
+            if(productsStore[findItemInArray(productsStore, currentId)].count || productsStore[findItemInArray(productsStore, currentId)].count > 1) {
+                let price = productItem.price
+                productItem.count -= 1
+                document.querySelector(`#price-${currentId}`).innerHTML = price * productsStore[findItemInArray(productsStore, currentId)].count
+                document.querySelector(`#count-${currentId}`).innerHTML = `Count: ${productsStore[findItemInArray(productsStore, currentId)].count}`
+            }
+        })
+    })
     let btnsAdd = document.querySelectorAll('.btnAdd')
     btnsAdd.forEach((btnItem) => { 
         btnItem.addEventListener('click', () => { 
             let currentId = parseInt(btnItem.parentNode.id.substring(8))
             console.log(currentId)
             let balanceCurrent = parseInt(localStorage.getItem('balance'))
-            let price = productsStore[findItemInArray(productsStore, currentId)].price
+            let productItem = productsStore[findItemInArray(productsStore, currentId)]
+            let price = 0
+            if (productItem.count){
+                price = productItem.price * productItem.count
+            } else { 
+                price = productsStore[findItemInArray(productsStore, currentId)].price
+            }
             if (balanceCurrent - price >= 0) {
                 bucketStore = [
                     ...bucketStore,
